51edcdf75db99f9c28856d0a826f743e4221a6f5
Maria CHEREL SOROKINA <mcherel-@paul-f5Ar4s1.clusters.42paris.fr>
2024-09-13 10:29:52 +0200

    first

---
+++ CS50/Problem Set 0/einstein/einstein.py
@@ -0,0 +1,6 @@
+def main():
+    s = input("m: ")
+    print(f"{int(s) * 90000000000000000}")
+
+if __name__ == "__main__":
+    main()
---
+++ CS50/Problem Set 0/faces/faces.py
@@ -0,0 +1,6 @@
+def main():
+    s = input()
+    print(s.replace(":)", "\N{slightly smiling face}").replace(":(", "\N{slightly frowning face}"))
+
+if __name__ == "__main__":
+    main()
